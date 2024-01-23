a0bb023dca374c1cf40e86daf68d0525cfba03b7
Ogollah <kogollah2011@gmail.com>
2024-01-23 12:00:04 +0300

    filter

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,13 +6,17 @@ returns the list of school having a specific topic
 
 def schools_by_topic(mongo_collection, topic):
     """
-    list of school having a specific topic
+    Returns a list of schools with a specific topic from a MongoDB collection.
+
+    Args:
+    - mongo_collection: pymongo collection object
+    - topic: string, the topic to search
+
+    Returns:
+    - List of schools with the specified topic.
     """
-    matcher = {
-        'topics': {
-            '$eleMatch': {
-                '$mtch': topic,
-            },
-        },
-    }
-    return [doc for doc in mongo_collection.find(matcher)]
+    query = {"topics": {"$in": [topic]}}
+    result = mongo_collection.find(query)
+
+    schools = [school for school in result]
+    return schools

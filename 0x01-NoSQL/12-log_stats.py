3baa0171523bad5bc4e49e649ceb1a477b5e04d4
Ogollah <kogollah2011@gmail.com>
2024-01-23 13:17:26 +0300

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,6 +7,22 @@ from pymongo import MongoClient
 
 
 def nginx_logs_stats():
+    """
+    Provides statistics about Nginx logs stored in MongoDB.
+
+    This function connects to the
+    MongoDB database "logs" and collection "nginx",
+    retrieves various statistics, and prints the results.
+
+    Displayed Statistics:
+    - Total number of logs
+    - Number of logs for each HTTP method (GET, POST, PUT, PATCH, DELETE)
+    - Number of logs with method=GET and path=/status
+
+    Returns:
+    None
+    """
+    # Connect to MongoDB
     client = MongoClient("mongodb://localhost:27017/")
     db = client["logs"]
     collection = db["nginx"]
